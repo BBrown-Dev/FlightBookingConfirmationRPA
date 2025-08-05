ccd1fda674868527fedcaf94519564f01477b01c
Bruce Brown <brown.bruce32@gmail.com>
2025-08-05 18:13:07 -0500

    Fix: coerce invalid fare values and fill NaNs with median

--- services/transformer.py
+++ services/transformer.py
@@ -1,8 +1,16 @@
 # This module contains functions to transform the DataFrame.
+from venv import logger
+import pandas as pd
+
 
 def convert_fares(df):
     # Converts Fare column to float.
-    df["Fare"] = df["Fare"].astype(float)
+    df["Fare"] = pd.to_numeric(df["Fare"], errors="coerce")
+    invalid_number = df["Fare"].isna().sum()
+
+    if invalid_number:
+        logger.warning(f"{invalid_number} invalid fares coerced to NaN")
+    df["Fare"].fillna(df["Fare"].median(), inplace=True)
     return df
 
 def add_total(df, tax_rate):
